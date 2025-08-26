28fe1ad2752bd4618b4b2af2708ae3e879be8f54
Anointeddrew <abah.d.andrew@gmail.com>
2025-08-26 08:08:17 +0100

    app file updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 import AdminMessages from "./pages/AdminMessages";
 import ProtectedRoute from "./Components/ProtectedRoute";
 import Login from "./pages/Login";
@@ -20,7 +20,6 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/services" element={<Services />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/admin/messages" element={<AdminMessages />} />
             <Route path="/admin/messages" element={<ProtectedRoute adminOnly><AdminMessages /></ProtectedRoute>} />
             <Route path="/login" element={<Login />} />
           </Routes>
